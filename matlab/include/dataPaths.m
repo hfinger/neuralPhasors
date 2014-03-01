1ba5ba3a5d6a5e1121ef3ce06c80163b1c8a8cc6
Holger Finger <hfinger@users.noreply.github.com>
2014-03-01 12:47:01 +0100

    "changed dataPaths to work for different srcFolder names."

--- matlab/include/dataPaths.m
+++ matlab/include/dataPaths.m
@@ -5,6 +5,11 @@ if ispc
 else
     basepath = '/net/store/nbp/phasesim';
 end
+
+p = mfilename('fullpath');
+p = strsplit(p,filesep);
+srcFolder = p{end-3};
+
 data.HOMEIMAGES = [basepath '/databases/LabelMeImages'];
 data.HOMEANNOTATIONS = [basepath '/databases/LabelMeAnnotations'];
 data.pdflatex = '/net/store/users/hofinger/texlive/2011/bin/x86_64-linux/pdflatex';
@@ -12,13 +17,13 @@ data.labelMeToolbox = [basepath '/databases/LabelMeToolbox'];
 data.cifar100matlab = [basepath '/databases/cifar-100-matlab'];
 data.databases = [basepath '/databases'];
 
-data.paramdir = [basepath '/src/matlab/parameters'];
+data.paramdir = [basepath '/' srcFolder '/matlab/parameters'];
 data.workdir = [basepath '/workdir'];
 data.resultsdir = [basepath '/results'];
 
 data.sge_workdir = ['/net/store/nbp/phasesim/workdir'];
 data.sge_resultsdir = ['/net/store/nbp/phasesim/results'];
-data.sge_pathToAddScriptPaths = ['/net/store/nbp/phasesim/src/matlab'];
+data.sge_pathToAddScriptPaths = ['/net/store/nbp/phasesim/' srcFolder '/matlab'];
 
 % data.plinkArg = '-ssh -i D:\Daten\putty\puttyUOS\putty_nopw.ppk hofinger@gate.ikw.uni-osnabrueck.de';
 % use putty gateway instead:
