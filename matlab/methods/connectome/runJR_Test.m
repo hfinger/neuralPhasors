cf23d394947144294e943219c32a3b8030b1c21b
Richard Gast <richgast.90@gmail.com>
2017-03-17 17:40:47 +0100

    added driver run time control and phase difference plot

--- matlab/methods/connectome/runJR_Test.m
+++ matlab/methods/connectome/runJR_Test.m
@@ -39,12 +39,14 @@ di2 = -1/Ti^2;
 S0 = WTP(0, e0, u0, r);
 
 % driver parameters
-drivStrength = 100;
+drivStrength = 0.1;
 drivFreq = 30;
+drivEnd = Tmax/2;
 
-%%
+%% Run simulation
 numIters = ceil(Tmax/dt);
 x_all = zeros(numIters,13);
+driv_all = zeros(numIters,1);
 a = 0;
 tic()
 t = 0;
@@ -54,6 +56,10 @@ for i=1:numIters
   x_all(i,:) = x;
   Pe = 220 + 22 * randn(1);
   Pp = drivStrength * sin(t*2*pi*drivFreq);
+  driv_all(i) = Pp;
+  if t > drivEnd
+      Pp = 0;
+  end
   
   [ dx, da ] = JansenRitUpdate( x, a, Pp, Pe, Ke, Ki, Ka, Cpe, Cpi, Cep, Cip, Cii, de1, de2, di1, di2, e0, u0, r, S0 );
   x = x + dx * dt;
@@ -61,7 +67,10 @@ for i=1:numIters
   
 end
 toc()
-%%
+
+%% plot stuff
+
+% spectrogram
 Fs = 1/dt;
 Hs = spectrum.periodogram;
 psd(Hs,x_all(:,1),'Fs',Fs)
@@ -76,7 +85,42 @@ line([0.05905,0.05905],[-90,0],'Color','g','LineStyle','-.')
 line([0.01,0.07],[-54.5,-54.5],'Color','g','LineStyle','-.')
 hold off
 
-% figure(2)
+% phase difference between driver and pyramidal cells
+env.t_rm = 1; % time to start bandpassing the signal at [s]
+env.sigBandpass(1).Fst1 = 25.5; % end stop band [Hz] 
+env.sigBandpass(1).Fp1 = 28; % start pass band [Hz]
+env.sigBandpass(1).Fp2 = 32; % [Hz] end pass band
+env.sigBandpass(1).Fst2 = 34.5; % [Hz] start stop band
+env.sigBandpass(1).Ast1 = 30; % frequency attenuation in first stopband
+env.sigBandpass(1).Ap = 1; % passband ripples
+env.sigBandpass(1).Ast2 = 30; % frequency attenuation in second stopband
+
+bp = env.sigBandpass(1);
+dBandPass = fdesign.bandpass('Fst1,Fp1,Fp2,Fst2,Ast1,Ap,Ast2',bp.Fst1,bp.Fp1,bp.Fp2,bp.Fst2,bp.Ast1,bp.Ap,bp.Ast2,Fs);
+HdBandPass = design(dBandPass,'butter');
+
+data = vertcat(x_all(:,1)',driv_all');
+sourceBP = zeros(size(data));
+for k=1:size(data,1)
+    sourceBP(k,:) = filter(HdBandPass,squeeze(data(k,:)),2);
+end
+sigHilbert = zeros(size(sourceBP));
+for n=1:size(sigHilbert,1)
+    sigHilbert(n,:) = hilbert(squeeze(sourceBP(n,:)));
+end
+phaseBP = angle(sigHilbert);
+phaseDiff = mod(bsxfun(@minus,phaseBP(1,:),phaseBP(2,:)), 2*pi);
+
+figure(2)
+xvec = 0:size(phaseDiff, 2) - 1;
+xvec = (xvec - drivEnd/dt)*dt;
+plot(xvec,phaseDiff)
+ylabel('phase')
+xlabel('Seconds since driver was turned off')
+title('Phase Difference between Driver and NMM')
+
+% PSPs of the 3 different cell populations
+% figure(3)
 % plot(0:dt:(Tmax-dt),x_all(:,1:3))
 % figure(2)
 % 
