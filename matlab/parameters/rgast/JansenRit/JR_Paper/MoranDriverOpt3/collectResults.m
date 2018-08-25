a1356548ce5d52ccd895222af1bd7406b744963a
Holger Finger <hfinger@users.noreply.github.com>
2018-08-25 18:58:35 +0200

    sddfsdf

---
+++ matlab/parameters/rgast/JansenRit/JR_Paper/MoranDriverOpt3/collectResults.m
@@ -0,0 +1,35 @@
+data = dataPaths();
+[~,my_foldername] = fileparts(pwd);
+path_results = fullfile(data.resultsdir, ['rgast/JansenRit/JR_Paper/' my_foldername]);
+
+jobDesc = load( fullfile(data.workdir, 'rgast/JansenRit/JR_Paper',my_foldername,'temp_Moran_Driver','jobDesc.mat') );
+
+paramComb = jobDesc.paramComb;
+variableParams = jobDesc.variableParams;
+numJobs = size(paramComb, 2);
+
+paramValues = cell(1, length(jobDesc.variableParams));
+for k=1:length(jobDesc.variableParams)
+    newStruct = jobDesc.params;
+    for f=1:length(jobDesc.variableParams{k})
+        fname = jobDesc.variableParams{k}{f};
+        newStruct = newStruct.(fname);
+    end
+    paramValues{k} = newStruct;
+end
+    
+%%
+
+all_coh = zeros(1,numJobs);
+for j=1:numJobs
+    fname = fullfile( path_results, ['Moran_Driver' num2str(j) '.mat']);
+    if exist(fname, 'file')
+        tmp = load( fname );
+        all_coh(j) = tmp.simResult.coh_of_roi_with_driver{1};
+    else
+        disp(['file missing: ' fname]);
+    end
+end
+
+save(fullfile( path_results, 'all_coh.mat'), 'all_coh', 'paramComb', 'variableParams', 'paramValues')
+
