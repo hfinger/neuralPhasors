ab82bda052d20bbac371e38194f882eaf7657dc9
Holger Finger <hfinger@users.noreply.github.com>
2018-12-01 01:54:19 +0100

    dsfsdf

---
+++ matlab/parameters/rgast/JansenRit/JR_Paper/DriverVarFreq4_unifNoise/printResults.m
@@ -0,0 +1,19 @@
+data = dataPaths();
+[~,my_foldername] = fileparts(pwd);
+path_results = fullfile(data.resultsdir, ['rgast/JansenRit/JR_Paper/' my_foldername]);
+
+results = load(fullfile( path_results, 'all_coh.mat'));
+
+paramSizes = cellfun(@length, results.paramValues);
+
+results.coh = squeeze(reshape(results.all_coh, paramSizes));
+
+strengths = cell2mat(results.paramValues{3});
+freqs = cell2mat(results.paramValues{4});
+
+
+imagesc(squeeze(mean(results.coh,1)))
+set(gca, 'XTick', 1:length(freqs), 'XTickLabel', freqs)
+set(gca, 'YTick', 1:length(strengths), 'YTickLabel', strengths)
+xlabel('Freq [Hz]')
+ylabel('Driver Strength [mV]')
