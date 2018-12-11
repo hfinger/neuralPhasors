38a91a24f86e61b2ecb532d0c980fecaeb74854c
Holger Finger <hfinger@users.noreply.github.com>
2018-12-11 12:31:24 +0100

    dfd

--- matlab/parameters/rgast/JansenRit/JR_Paper/DriverVarFreq4_unifNoise/collectResults.m
+++ matlab/parameters/rgast/JansenRit/JR_Paper/DriverVarFreq4_unifNoise/collectResults.m
@@ -19,17 +19,25 @@ for k=1:length(jobDesc.variableParams)
 end
     
 %%
-
 all_coh = zeros(1,numJobs);
+all_FC = cell(1,numJobs);
+all_corr_SimFC = cell(1,numJobs);
 for j=1:numJobs
-    fname = fullfile( path_results, ['Connectome' num2str(j) '.mat']);
-    if exist(fname, 'file')
-        tmp = load( fname );
+  fname = fullfile( path_results, ['Connectome' num2str(j) '.mat']);
+  if exist(fname, 'file')
+      tmp = load( fname );
+
+      all_FC{j} = tmp.simResult.FC;
+      if jobDesc.params.JansenRitConnectomePaper.corrSimFC
+        all_corr_SimFC{j} = tmp.simResult.corr_SimFC;
+      end
+
+      if jobDesc.params.JansenRitConnectomePaper.calcCohWithDriver
         all_coh(j) = tmp.simResult.coh_of_roi_with_driver{1};
-    else
-        disp(['file missing: ' fname]);
-    end
+      end
+  else
+      disp(['file missing: ' fname]);
+  end
 end
 
-save(fullfile( path_results, 'all_coh.mat'), 'all_coh', 'paramComb', 'variableParams', 'paramValues')
-
+save(fullfile( path_results, 'all_coh.mat'), 'all_coh', 'paramComb', 'variableParams', 'paramValues', 'all_FC', 'all_corr_SimFC')
