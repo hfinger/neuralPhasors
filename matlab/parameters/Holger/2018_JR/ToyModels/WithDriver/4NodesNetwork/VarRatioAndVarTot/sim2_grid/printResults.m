287663a8e4318da1e0848b728f55fb366e65af00
Holger Finger <hfinger@users.noreply.github.com>
2019-03-02 11:16:09 +0100

    4 nodes network

---
+++ matlab/parameters/Holger/2018_JR/ToyModels/WithDriver/4NodesNetwork/VarRatioAndVarTot/sim2_grid/printResults.m
@@ -0,0 +1,27 @@
+clear all;
+
+data = dataPaths();
+[~,my_foldername] = fileparts(pwd);
+path_results = fullfile(data.resultsdir, ['Holger/2018_JR/ToyModels/WithDriver/3NodesBidirect/' my_foldername]);
+results = load(fullfile( path_results, 'all_coh.mat'));
+
+paramSizes = cellfun(@length, results.paramValues);
+
+phase_offsets = [0:0.0625:1]*2*pi;
+d213 = 0:5:200;
+
+%%
+stim_pair_coh = zeros(length(results.all_FC),16);
+for k=1:length(results.all_FC)
+    FC = results.all_FC{k}{1}(33:end,33:end);
+    FC_per_SPO = diag(FC,1);
+    stim_pair_coh(k,:) = FC_per_SPO(2:3:end);
+end
+stim_pair_coh = cat(2, stim_pair_coh, stim_pair_coh(:,1));
+
+%%
+imagesc(phase_offsets,d213,stim_pair_coh)
+xlabel('stimulation phase offset [rad]')
+ylabel('distance [mm]')
+set(gca,'YDir','normal')
+colorbar;
