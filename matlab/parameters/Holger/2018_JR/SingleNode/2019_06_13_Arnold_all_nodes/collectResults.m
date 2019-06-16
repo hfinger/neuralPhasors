f0b44f09ecce65395242fccf7022dde3a7beb507
Holger Finger <hfinger@users.noreply.github.com>
2019-06-16 13:49:33 +0200

    output plots of arnold tongue etc

--- matlab/parameters/Holger/2018_JR/SingleNode/2019_06_13_Arnold_all_nodes/collectResults.m
+++ matlab/parameters/Holger/2018_JR/SingleNode/2019_06_13_Arnold_all_nodes/collectResults.m
@@ -1,13 +1,15 @@
 data = dataPaths();
 [~,my_foldername] = fileparts(pwd);
-path_results = fullfile(data.resultsdir, ['rgast/JansenRit/JR_Paper/' my_foldername]);
+path_results = fullfile(data.resultsdir, ['Holger/2018_JR/SingleNode/' my_foldername]);
+path_workdir = fullfile(data.workdir, ['Holger/2018_JR/SingleNode/' my_foldername]);
 
-jobDesc = load( fullfile(data.workdir, 'rgast/JansenRit/JR_Paper',my_foldername,'temp_Connectome','jobDesc.mat') );
+jobDesc = load( fullfile(data.workdir, 'Holger/2018_JR/SingleNode',my_foldername,'temp_Connectome','jobDesc.mat') );
 
 paramComb = jobDesc.paramComb;
 variableParams = jobDesc.variableParams;
 params = jobDesc.params;
 numJobs = size(paramComb, 2);
+numRepeat = length(jobDesc.params.JansenRitConnectomePaper.drivPosVarMatrix);
 
 paramValues = cell(1, length(jobDesc.variableParams));
 for k=1:length(jobDesc.variableParams)
@@ -20,25 +22,37 @@ for k=1:length(jobDesc.variableParams)
 end
     
 %%
-all_coh = zeros(1,numJobs);
-all_FC = cell(1,numJobs);
-all_corr_SimFC = cell(1,numJobs);
+all_coh = zeros(1,numJobs,numRepeat);
+all_FC = cell(1,numJobs,numRepeat);
+all_corr_SimFC = cell(1,numJobs,numRepeat);
+all_meanKuramotoOrderParam = zeros(1,numJobs,numRepeat);
 for j=1:numJobs
-  fname = fullfile( path_results, ['Connectome' num2str(j) '.mat']);
+  fname = fullfile( path_workdir, ['Connectome' num2str(j) '.mat']);
   if exist(fname, 'file')
       tmp = load( fname );
 
-      all_FC{j} = tmp.simResult.FC;
-      if jobDesc.params.JansenRitConnectomePaper.corrSimFC
-        all_corr_SimFC{j} = tmp.simResult.corr_SimFC;
-      end
+      for k=1:numRepeat
+        all_FC{1,j,k} = tmp.simResult{k}.FC;
+        if jobDesc.params.JansenRitConnectomePaper.corrSimFC
+          all_corr_SimFC{1,j,k} = tmp.simResult{k}.corr_SimFC;
+        end
 
-      if jobDesc.params.JansenRitConnectomePaper.calcCohWithDriver
-        all_coh(j) = tmp.simResult.coh_of_roi_with_driver{1};
+        if jobDesc.params.JansenRitConnectomePaper.calcCohWithDriver
+          all_coh(1,j,k) = tmp.simResult{k}.coh_of_roi_with_driver{1};
+        end
+        
+        if jobDesc.params.JansenRitConnectomePaper.calcKuramotoOrderParam
+          all_meanKuramotoOrderParam(1,j,k) = tmp.simResult{k}.meanKuramotoOrderParam;
+        end
+        
       end
+      
   else
       disp(['file missing: ' fname]);
   end
 end
 
-save(fullfile( path_results, 'all_coh.mat'), 'all_coh', 'params', 'paramComb', 'variableParams', 'paramValues', 'all_FC', 'all_corr_SimFC')
+if ~isfolder(path_results)
+  mkdir(path_results)
+end
+save(fullfile( path_results, 'all_coh.mat'), 'all_coh', 'params', 'paramComb', 'variableParams', 'paramValues', 'all_FC', 'all_corr_SimFC', 'all_meanKuramotoOrderParam')
--- matlab/parameters/Holger/2018_JR/SingleNode/2019_06_13_Arnold_all_nodes/printResults.m
+++ matlab/parameters/Holger/2018_JR/SingleNode/2019_06_13_Arnold_all_nodes/printResults.m
@@ -1,12 +1,15 @@
 data = dataPaths();
 [~,my_foldername] = fileparts(pwd);
-path_results = fullfile(data.resultsdir, ['rgast/JansenRit/JR_Paper/' my_foldername]);
+path_results = fullfile(data.resultsdir, ['Holger/2018_JR/SingleNode/' my_foldername]);
 
 results = load(fullfile( path_results, 'all_coh.mat'));
 
 paramSizes = cellfun(@length, results.paramValues);
+numRepeat = length(results.params.JansenRitConnectomePaper.drivPosVarMatrix);
 
+paramSizes = [paramSizes numRepeat];
 results.coh = squeeze(reshape(results.all_coh, paramSizes));
+results.meanKuramotoOrderParam = squeeze(reshape(results.all_meanKuramotoOrderParam, paramSizes));
 results.FC = squeeze(reshape(results.all_FC, paramSizes));
 results.FC = cellfun(@(x) x{1}, results.FC, 'UniformOutput', false);
 results.coh_driv_to_net = cell2mat(cellfun(@(x) mean(x(1,2:end)), results.FC, 'UniformOutput', false));
@@ -15,12 +18,13 @@ C = results.params.JansenRitConnectomePaper.C;
 C = C' + C;
 C = C > 0;
 
-driver_node_idx = cell2mat(results.paramValues{2});
-strengths = cell2mat(results.paramValues{3});
-freqs = cell2mat(results.paramValues{4});
+driver_node_idx = results.params.JansenRitConnectomePaper.drivPosVarMatrix;
+strengths = cell2mat(results.paramValues{2});
+freqs = cell2mat(results.paramValues{3});
 
-driver_coh = squeeze(mean(results.coh,1));
-driver_coh_to_net = squeeze(mean(results.coh_driv_to_net,1));
+driver_coh = squeeze(mean(results.coh,3));
+driver_meanKuramotoOrderParam = squeeze(mean(results.meanKuramotoOrderParam,3));
+driver_coh_to_net = squeeze(mean(results.coh_driv_to_net,3));
 
 %%
 figure(1);
@@ -32,9 +36,9 @@ colorbar;
 xlabel('Freq [Hz]')
 ylabel('Driver Strength [mV]')
 title('coherence to driven node')
-saveas(gcf,'coh_to_driven_node.png')
+saveas(gcf,fullfile(path_results, 'coh_to_driven_node.png'))
 
-figure(6);
+figure(2);
 imagesc(driver_coh_to_net)
 set(gca, 'XTick', 1:2:length(freqs))
 set(gca, 'XTickLabel', freqs(1:2:end))
@@ -43,7 +47,18 @@ colorbar;
 xlabel('Freq [Hz]')
 ylabel('Driver Strength [mV]')
 title('mean coherence to all network nodes')
-saveas(gcf,'mean_coh_to_net.png')
+saveas(gcf,fullfile(path_results, 'mean_coh_to_net.png'))
+
+figure(3);
+imagesc(driver_meanKuramotoOrderParam)
+set(gca, 'XTick', 1:2:length(freqs))
+set(gca, 'XTickLabel', freqs(1:2:end))
+set(gca, 'YTick', 1:length(strengths), 'YTickLabel', strengths)
+colorbar;
+xlabel('Freq [Hz]')
+ylabel('Driver Strength [mV]')
+title('mean kuramoto order param of network')
+saveas(gcf,fullfile(path_results, 'mean_kuramoto_order_param.png'))
 
 %%
 target_freq = 11;
@@ -53,7 +68,7 @@ driver_idx = 1;
 target_freq_idx = find(freqs == target_freq);
 target_driver_strength_idx = find(driver_coh(:,target_freq_idx) > target_coh, 1);
 
-FC = results.FC{driver_idx, target_driver_strength_idx, target_freq_idx};
+FC = results.FC{target_driver_strength_idx, target_freq_idx, driver_idx};
 cur_driver_node_idx = driver_node_idx(driver_idx);
 coh_roi_with_driver = FC(2:end,1);
 coh_roi_with_roi = FC(2:end,2:end);
@@ -63,23 +78,23 @@ coh_roi_with_net = (sum(coh_roi_with_roi,2)-1)/(size(coh_roi_with_roi,2)-1);
 coh_roi_with_driver = coh_roi_with_driver(I);
 coh_roi_with_net = coh_roi_with_net(I);
 
-figure(2)
+figure(4)
 plot([coh_roi_with_driver, coh_roi_with_net])
 
-figure(3); 
+figure(5); 
 imagesc(FC)
 
 %%
 coh_driv_to_nn = zeros(size(results.FC));
-for driver_idx=1:size(results.FC,1)
+for driver_idx=1:size(results.FC,3)
     cur_driver_node_idx = driver_node_idx(driver_idx);
     all_nn_idxs = find(C(cur_driver_node_idx,:));
-    tmp = cellfun(@(x) x(1,2:end), results.FC(driver_idx, :, :), 'UniformOutput', false);
-    coh_driv_to_nn(driver_idx, :, :) = cell2mat(cellfun(@(x) mean(x(all_nn_idxs)), tmp, 'UniformOutput', false));
+    tmp = cellfun(@(x) x(1,2:end), results.FC(:, :, driver_idx), 'UniformOutput', false);
+    coh_driv_to_nn(:, :, driver_idx) = cell2mat(cellfun(@(x) mean(x(all_nn_idxs)), tmp, 'UniformOutput', false));
 end
 
-figure(9);
-imagesc(squeeze(mean(coh_driv_to_nn,1)))
+figure(6);
+imagesc(squeeze(mean(coh_driv_to_nn,3)))
 set(gca, 'XTick', 1:2:length(freqs))
 set(gca, 'XTickLabel', freqs(1:2:end))
 set(gca, 'YTick', 1:length(strengths), 'YTickLabel', strengths)
@@ -87,4 +102,4 @@ colorbar;
 xlabel('Freq [Hz]')
 ylabel('Driver Strength [mV]')
 title('mean coherence to nearest neigbors in network')
-saveas(gcf,'mean_coh_to_nn.png')
+saveas(gcf,fullfile(path_results, 'mean_coh_to_nn.png'))
