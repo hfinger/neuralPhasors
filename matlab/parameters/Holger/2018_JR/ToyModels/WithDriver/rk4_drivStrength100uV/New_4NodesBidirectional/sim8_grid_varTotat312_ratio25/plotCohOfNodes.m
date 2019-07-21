5073591606d4eec91b1ab9480ed70a8acbeed120
Holger Finger <hfinger@users.noreply.github.com>
2019-07-21 14:37:24 +0200

    new simulation with 4 nodes

---
+++ matlab/parameters/Holger/2018_JR/ToyModels/WithDriver/rk4_drivStrength100uV/New_4NodesBidirectional/sim8_grid_varTotat312_ratio25/plotCohOfNodes.m
@@ -0,0 +1,20 @@
+function plotCohOfNodes(FC_sum,node1,node2, d12, phase_offsets, path_results, minC, maxC)
+%PLOTCOHOFNODES Summary of this function goes here
+%   Detailed explanation goes here
+
+%figure(1)
+imagesc(d12,phase_offsets,squeeze(FC_sum(:,:,node1,node2))')
+set(gca,'clim',[minC; maxC]);
+set(gca,'TickLength',[0 0]);
+set(gca,'xTick',[0, 200]);
+set(gca,'yTick',[0]);
+xlabel('distance [mm]')
+ylabel('stimulation phase offset [rad]')
+set(gca,'YDir','normal')
+title(['coh node ' num2str(node1) ' to node ' num2str(node2)])
+colorbar;
+saveas(gcf,fullfile(path_results,['coh_' num2str(node1) num2str(node2) '.png']))
+saveas(gcf,fullfile(path_results,['coh_' num2str(node1) num2str(node2) '.pdf']))
+
+end
+
